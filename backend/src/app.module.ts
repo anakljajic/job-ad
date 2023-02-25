5428a013f1f20b980d5cbf0a9e118f326ec84efb
anakljajic <kljajic.ana@yahoo.com>
2023-02-25 12:29:10 +0100

    Application dockerized

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,28 +1,25 @@
 import { Module } from '@nestjs/common';
-import { JobAdModule } from './job-ad/job-ad.module';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { JobAd } from './entities';
+import entities from './entities';
+import { HttpModule } from '@nestjs/axios';
+import { JobAdModule } from './job-ad/job-ad.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot({ isGlobal: true }),
     TypeOrmModule.forRootAsync({
       imports: [ConfigModule],
-      useFactory: (configService: ConfigService) => ({
+      useFactory: () => ({
         type: 'postgres',
-        host: configService.get('DB_HOST'),
-        port: +configService.get<number>('DB_PORT'),
-        username: configService.get('DB_USERNAME'),
-        password: configService.get('DB_PASSWORD'),
-        database: configService.get('DB_NAME'),
-        autoLoadEntities: true,
-        entities: [JobAd],
+        url: process.env.DATABASE_URL,
+        entities: entities,
         synchronize: true,
       }),
       inject: [ConfigService],
     }),
     JobAdModule,
+    HttpModule,
   ],
   controllers: [],
   providers: [],
